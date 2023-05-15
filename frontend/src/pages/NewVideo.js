75105d2e5177925a257f40e1755f7633b6573624
Ty <Ty@Tys-Macbook.local>
2023-05-15 14:16:16 -0400

    foundation started

---
+++ frontend/src/pages/NewVideo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './NewVideo.css'
+		  
+function NewVideo() {
+    return (
+		<div className="newvideo">
+			New Video Page
+		</div>
+	)
+}
+		  
+export default NewVideo
+		  
