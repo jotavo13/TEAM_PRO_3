7919282fb1c04a15570ac7c936d6914a1d1625d3
jotavo13 <122843130+jotavo13@users.noreply.github.com>
2023-05-21 18:26:37 +0000

    CSS on Vdieo.js and Video.css

--- frontend/src/components/Video.js
+++ frontend/src/components/Video.js
@@ -6,25 +6,29 @@ function Video({ video }) {
   const timeFromEvent = console.log(video);
   return (
     <NavLink to={video.videoURL} target="_">
-		<div class="grid-container">
-      <div className="video">
-        <div class="card" style={{width: "18rem"}}>
-        <img src={video.thumbnail} alt="" className="thumbnail" />
+      <div class="grid-container">
+        <div className="video">
+          <div class="card" style={{ width: "18rem" }}>
+            <img src={video.thumbnail} alt="" className="thumbnail" />
+          </div>
+          <br></br>
+          <div class="row">
+          <div class="col-sm-6 bg text-white"style={{ width: "25%", height: "25%", backGround: "none"}}>
+          <img src={video.channelThumbnail} alt="" className="channelThum" />
+          </div>
+          <div class="col-sm-6 bgtext-white" style={{ width: "75%", height: "25%"}} >
+          <h3 id="topTitle">{video.title}</h3>
+          <h4 id="midTitle">{video.channelTitle}</h4>
+          <span className="inline">
+            <h4>{video.views} views </h4>
+          </span>
+          <span className="inline">
+            <h4>{video.publishTime}</h4>
+          </span>
+          </div>
+            </div>
         </div>
-
-        {/* <img src={video.thumbnail} alt="" className="thumbnail" /> */}
-        <br></br>
-        <img src={video.channelThumbnail} alt="" className="channelThum" />
-        <h3>{video.title}</h3>
-        <h4>{video.channelTitle}</h4>
-        <span className="inline">
-          <h4>{video.views}   views </h4>
-        </span>
-        <span className="inline">
-          <h4>{video.publishTime}</h4>
-        </span>
       </div>
-	  </div>
     </NavLink>
   );
 }
