18487c832a6e95bcb09cbef1ae2cd1492287b07c
Ty <Ty@Tys-Macbook.local>
2023-06-01 10:13:47 -0400

    date format

--- frontend/src/components/Video.js
+++ frontend/src/components/Video.js
@@ -23,17 +23,98 @@ function formatNumber(number) {
 
 
 
-const dateNew = new Date(video.publishTime);
 
 
-const hours = dateNew.getHours();
-const minutes = dateNew.getMinutes();
-const seconds = dateNew.getSeconds();
+
+const formatDate = (date) => {
+
+  let relativeTime;
+
+  let dateNew = new Date();
 
 
-const dateNormal = hours.toString().padStart(2, '0') + ':' +
-                    minutes.toString().padStart(2, '0') + ':' +
-                    seconds.toString().padStart(2, '0');
+  let todaysDay = dateNew.getDate();
+  let todaysYear = dateNew.getFullYear();
+  let todaysMonth = dateNew.getMonth();
+  let todaysHour = dateNew.getHours();
+  let todaysMinute = dateNew.getMinutes();
+
+  let videoYear = +date.slice(0,4);
+  let videoMonth = +date.slice(5,7) - 1;
+  let videoDay = +date.slice(8,10);
+  let videoHour = +date.slice(11,13);
+  let videoMinute = +date.slice(14,16);
+  console.log(date);
+  console.log(todaysDay, videoDay);
+
+  console.log('date', dateNew.getDate(), videoDay, dateNew.getFullYear(), videoYear, dateNew.getMonth(), videoMonth, todaysHour);
+
+
+  if(todaysYear > videoYear){
+    relativeTime = videoYear - todaysYear;
+    relativeTime *= relativeTime;
+    if(relativeTime == 1){
+      return `${relativeTime.toString()} year ago`;
+    }
+    else{
+      return `${relativeTime.toString()} years ago`;
+    }
+  }
+  else if(todaysMonth > videoMonth){
+    relativeTime = videoMonth - todaysMonth;
+    relativeTime *= relativeTime;
+    if(relativeTime == 1){
+      return `${relativeTime.toString()} month ago`;
+    }
+    else {
+      return `${relativeTime.toString()} months ago`;
+    }
+  }
+  else if(todaysDay > videoDay){
+    relativeTime = videoDay - todaysDay;
+    relativeTime *= relativeTime;
+    if(relativeTime == 1){
+      return `${relativeTime.toString()} day ago`;
+    }
+    else {
+      return `${relativeTime.toString()} days ago`;
+    }
+  }
+  else if(todaysHour > videoHour){
+    console.log(todaysHour, videoHour)
+
+    relativeTime = videoHour - todaysHour;
+    relativeTime *= relativeTime;
+    if(relativeTime == 1){
+      return `${relativeTime.toString()} hour ago`;
+    }
+    else {
+      return `${relativeTime.toString()} hours ago`;
+    }
+  }
+  else{
+    console.log(videoMinute, todaysMinute)
+    relativeTime = videoMinute - todaysMinute;
+    relativeTime *= relativeTime;
+    if(relativeTime == 1){
+      return `${relativeTime.toString()} minute ago`;
+    }
+    else {
+      return `${relativeTime.toString()} minutes ago`;
+    }
+  }
+
+
+
+
+}
+
+
+const dateNormal = '';
+
+// hours.toString().padStart(2, '0') + ':' +
+//                     minutes.toString().padStart(2, '0') + ':' +
+//                     seconds.toString().padStart(2, '0');
 
 
   return (
@@ -59,7 +140,7 @@ const dateNormal = hours.toString().padStart(2, '0') + ':' +
             <h4 >{formatNumber(video.views)} views     </h4>
           </span>
           <span className="inline date">
-            <h4>  {dateNormal}</h4>
+            <h4>  {formatDate(video.publishTime)}</h4>
           </span>
           </div>
             </div>
